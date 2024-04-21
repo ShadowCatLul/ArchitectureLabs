d879680462678dd094e936677ac33280d55a5304
ShadowCatLul <xivx@mail.ru>
2024-04-21 18:20:54 +0300

    лабы 2-3-5 объединены в одну папку, добавлена ЛР5

--- LR2_3_5/app/routers.py
+++ LR2_3_5/app/routers.py
@@ -12,7 +12,7 @@ from typing import Optional
 import jwt
 import hashlib
 from datetime import timedelta, datetime
-
+import redis
 
 def get_db():
     db = SessionLocal()
@@ -36,7 +36,7 @@ def check_user_token(auth: str = Header(...)):
 
     return user_id
 
-
+redis_db = redis.Redis(host='redis', port=6379, db=0)
 @router.post("/login")
 async def login(creds: HTTPBasicCredentials = Depends(HTTPBasic()), db: Session = Depends(get_db)):
     _user = read.get_user_by_login(db, creds.username)
@@ -76,6 +76,12 @@ async def get_user_by_first_name(request: str, db: Session = Depends(get_db)):
     key = f'get_by_first_name: {request}'
     _user = read.get_user_by_first_name(db, first_name=request)
 
+    redis_db.rpush(key, _user.first_name)
+    redis_db.expire(key, 30)
+
+
+
+
 
     return {
         "message"    : f"user '{_user.first_name}' found in postgres",
@@ -88,6 +94,9 @@ async def get_by_last_name(request: str, db: Session = Depends(get_db)):
 
     _user = read.get_user_by_last_name(db, last_name=request)
 
+    key = f'get_by_last_name: {request}'
+    redis_db.rpush(key, _user.last_name)
+    redis_db.expire(key, 30)
     return {
         "message"    : f"user '{_user.last_name}' found in postgres",
         "user"       : _user,
@@ -98,8 +107,15 @@ async def get_by_last_name(request: str, db: Session = Depends(get_db)):
 @router.get("/get_all")
 async def get_all(start: int, end: int, db: Session = Depends(get_db)):
 
-    _users = read.get_user(db, skip=start, limit=end)
 
+    key = f'get_all?start={start}&end={end}'
+    search = redis_db.lrange(key, 0, -1)
+    if redis_db.lrange(key, 0, -1): return f'redic_search{search}'
+
+    #всех юзеровв, который попали в область ключа нужно закинуть поближе в бд
+    redis_db.rpush(key,  *[str(u.id) for u in _users])
+    redis_db.expire(key, 30)
+    _users = read.get_user(db, skip=start, limit=end)
     return {
         "message"    : f"users found",
         "users"      : _users,
@@ -109,6 +125,14 @@ async def get_all(start: int, end: int, db: Session = Depends(get_db)):
 
 @router.get("/get_user_by_id")
 async def get_all(user_id: int, db: Session = Depends(get_db)):
+    key = f'get_user_by_id: {user_id}'
+    redis_search = redis_db.lrange(key, 0, -1)
+    if redis_search:
+        return {
+            "message": f"user '{redis_search}' found in Redis",
+            "status_code": "200"
+        }
+
     _user = read.get_user_by_id(db, user_id)
 
     return _user
@@ -122,6 +146,9 @@ async def update_user(user_id: int, new_first_name: str, new_second_name: str,
     _user = update.update_user(db, user_id=user_id, first_name=new_first_name, second_name=new_second_name,
                              password=new_password, login=new_login,email=new_email,is_active=new_status_is_active,
                              is_superuser=new_status_is_superuser, is_verified=new_status_is_verified)
+    
+    
+
     return {
         "message"    : f"user {user_id} update",
         "users"      : _user,
